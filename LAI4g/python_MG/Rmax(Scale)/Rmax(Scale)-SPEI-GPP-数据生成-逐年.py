77e125ac7464c0464ec849a6e6d5a6dd137dae55
Mayut2022 <98396768+Mayut2022@users.noreply.github.com>
2023-06-29 14:11:08 +0800

    Delete LAI4g directory

--- LAI4g/python_MG/Rmax(Scale)/Rmax(Scale)-SPEI-GPP-数据生成-逐年.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Mar 29 12:39:01 2023
-
-@author: MaYutong
-"""
-
--- LAI4g/python_MG/植被特征/LAI-空间趋势图-总体-分段 - 逐月.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Mar 30 13:43:26 2023
-
-@author: MaYutong
-"""
-
